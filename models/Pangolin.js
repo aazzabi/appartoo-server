afc739d02ae526b0465ee67b7324b10776ddcf35
Arafet <arafet.azzabi@esprit.tn>
2020-11-11 21:36:52 +0100

    Model Pangolin

---
+++ models/Pangolin.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var pangolinSchema = mongoose.Schema(
+    {
+        name: {type: String, unique: false, required: true, text: true},
+        pseudo: {type: String, unique: true, required: true, index: true, text: true},
+        password: {type: String, unique: false, required: true, text: true},
+        breed: {type: String, unique: false, text: true},
+        pangolins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Pangolin', required: false }],
+        weight: {type: Number, required: true},
+    });
+var pangolin = mongoose.model('Pangolin', pangolinSchema);
+module.exports = pangolin;
